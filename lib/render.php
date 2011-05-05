2281471d9c6b8ea5ca00d90a34196117463aff4d
Petr Babicka <babcca@gmail.com>
2011-05-05 18:40:31 +0200

    Vlastni remplate slozka pro aplikaci

--- lib/render.php
+++ lib/render.php
@@ -2,19 +2,19 @@
 require_once(dirname(__file__).'/smarty/Smarty.class.php');
 
 class Render extends Smarty {
-  private $aplication;
-  public function __construct($folder = '') {
-    parent::__construct();
-    $this->aplication = $folder;
-    $this->template_dir = './html/templates/'.$folder;
-    $this->compile_dir ='./html/templates_c';
-    $this->config_dir = './html/configs';
-    $this->cache_dir = './html/cache';
-  }
-  public function fetch($view = '') {
-    // kontrola $view
-    if (!$view) $view = $this->aplication;
-    return parent::fetch($view.".tpl");
-  }
+	private $aplication;
+	public function __construct($folder = '') {
+		parent::__construct();
+		$this->aplication = $folder;
+		$this->template_dir = $this->application;
+		$this->compile_dir =$this->application.'/templates_c';
+		$this->config_dir = $this->application.'/configs';
+		$this->cache_dir = $this->application.'/cache';
+	}
+	public function fetch($view = '') {
+		// kontrola $view
+		if (!$view) $view = $this->aplication;
+		return parent::fetch($view.".tpl");
+	}
 }
 ?>
