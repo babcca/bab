ccfb9700d579d5e5432c919640730f3c523b1e92
Petr Babicka <babcca@gmail.com>
2011-05-05 19:15:02 +0200

    prirazeni template slozky k aplikaci

--- lib/render.php
+++ lib/render.php
@@ -1,20 +1,25 @@
 <?php
 require_once(dirname(__file__).'/smarty/Smarty.class.php');
 
+/* 
+ * Render pro aplikace (smarty)
+ * Slouzi pro vykresleni view
+ */
 class Render extends Smarty {
-	private $aplication;
+	private $application;
 	public function __construct($folder = '') {
 		parent::__construct();
-		$this->aplication = $folder;
+		$this->application = Main::$config->main->app_dir.'/'.$folder;
 		$this->template_dir = $this->application;
 		$this->compile_dir =$this->application.'/templates_c';
 		$this->config_dir = $this->application.'/configs';
 		$this->cache_dir = $this->application.'/cache';
 	}
-	public function fetch($view = '') {
+	/* Vykresleni templatu ze slozky apliakce */
+	public function show($view = '') {
 		// kontrola $view
-		if (!$view) $view = $this->aplication;
-		return parent::fetch($view.".tpl");
+		if ($view) return parent::fetch($view.".tpl");
+		return '';
 	}
 }
 ?>
