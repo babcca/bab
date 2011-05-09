0fc545c523eb61553f3a6ab9727fe7aa2ba1a8bd
Petr Babicka <babcca@gmail.com>
2011-05-09 22:10:15 +0200

    Upraven vstupni bootstrap, index zaveden jako aplikace, ne jako soucast enginu

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 
 /* Vlozeni dulezitych knihoven */
 require_once(dirname(__file__).'/lib/main.php');
-require_once(dirname(__file__).'/lib/index.php');
 
 /* Inicializace prostredi */
 Main::__init__('config.ini');
 
 /* Registrace aplikaci */
+Main::$application_manager->register('index');
 Main::$application_manager->register('basic_page');
 
 /* Zobrazeni */
-new Index();
+echo Main::$process_manager->get_view(Main::$config->main->main_app, "main");
+
 
 ?>
