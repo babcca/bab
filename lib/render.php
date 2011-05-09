0b0e60a46fbeebb0ace50642ffae4511802d5f2d
Petr Babicka <babcca@gmail.com>
2011-05-09 16:18:32 +0200

    Pridany komentare

--- lib/render.php
+++ lib/render.php
@@ -1,22 +1,30 @@
 <?php
 require_once(dirname(__file__).'/smarty/Smarty.class.php');
 
-/* 
- * Render pro aplikace (smarty)
- * Slouzi pro vykresleni view
+/** 
+ *	Render pro aplikace (smarty)
+ * 	Slouzi pro vykresleni view casti aplikace
  */
 class Render extends Smarty {
-	private $application;
-	public function __construct($folder = '') {
+	private $application; /**< Slozka plikace */
+	/**
+	 * 	Inicializace smarty, kazda aplikace ma svoji slozku pro templaty
+	 *	\param $folder Slozka aplikace, kde je umistena slozka template se smarty strukturou
+	 */
+	public function __construct($folder) {
 		parent::__construct();
-		$this->application = Main::$config->main->app_dir.'/'.$folder;
-		$this->template_dir = $this->application;
+		$this->application = Main::$config->main->app_dir.'/'.$folder.'/templates';
+		$this->template_dir = $this->application.'/templates';
 		$this->compile_dir =$this->application.'/templates_c';
 		$this->config_dir = $this->application.'/configs';
 		$this->cache_dir = $this->application.'/cache';
 	}
-	/* Vykresleni templatu ze slozky apliakce */
-	public function show($view = '') {
+
+	/**
+	 *	Vykresleni templatu ze slozky apliakce
+	 *	\param $view Nazev templatu (bez koncovky .tpl)
+	 */
+	public function show($view) {
 		// kontrola $view
 		if ($view) return parent::fetch($view.".tpl");
 		return '';
