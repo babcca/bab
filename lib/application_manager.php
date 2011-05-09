81dd72900a0e36df78c6de4affd8775af0f9fda7
Petr Babicka <babcca@gmail.com>
2011-05-09 20:23:33 +0200

    Pridany komentare

--- lib/application_manager.php
+++ lib/application_manager.php
@@ -1,11 +1,25 @@
 <?php
+
+/**
+ * Spravce registrovanych aplikaci, kazda aplikace se musi zaregistrovat,
+ * aby bylo mozne ji volat z venku
+ */
+
 class AppManager {
-	private $_APP_LIST;
+	private $_APP_LIST; /**< Seznam registrovanych aplikaci */
 	
+	/**
+	 * Registrace aplikace
+	 * \param $app Nazev aplikace
+	 */
 	public function register($app) {
 		$this->_APP_LIST[$app] = false;
 	}
 	
+	/**
+	 * Importovani aplikace, system importuje jen aplikace ktere jsou potreba
+	 * \param $app Nazev importovane aplikace
+	 */
 	public function import($app) {
 		if (array_key_exists($app, $this->_APP_LIST)) {
 			if (!$this->_APP_LIST[$app]) {
