beb3466da917845aaf4afa827a4b65b54aed68be
Petr Babicka <babcca@gmail.com>
2011-05-12 21:49:53 +0200

    Pridan blugin do smary pro ziskani pred jakou dobou od urciteho casu

--- lib/render.php
+++ lib/render.php
@@ -18,6 +18,7 @@ class Render extends Smarty {
 		$this->compile_dir =$this->application.'/templates_c';
 		$this->config_dir = $this->application.'/configs';
 		$this->cache_dir = $this->application.'/cache';
+		$this->registerPlugin("function", "before_time", "Render::get_before_time");
 	}
 
 	/**
@@ -28,6 +29,29 @@ class Render extends Smarty {
 		// kontrola $view
 		if ($view) return parent::fetch($view.".tpl");
 		return '';
+	}
+
+	public static function get_before_time($params, $smarty) {
+		$r = array();
+		$r["s"] = (int) time() - $params["time"];
+		$r["m"] = (int) ($r["s"] / 60);
+		$r["h"] = (int) ($r["m"] / 60);
+		$r["d"] = (int) ($r["h"] / 24);
+		
+		if ($r["d"] != 0) {
+			if ($r["d"] == 1) return "dnem";
+			else return $r["d"]." dny";
+		} else 	if ($r["h"] != 0) {
+			if ($r["h"] == 1) return "hodinou";
+			else return $r["h"]." hodinami";
+		} else 	if ($r["m"] != 0) {
+			if ($r["m"] == 1) return "minutou";
+			else return $r["m"]." minutami";
+		} else 	if ($r["s"] != 0) {
+			if ($r["s"] == 1) return "vterinou";
+			else return $r["s"]." vterinami";
+		} 
+		
 	}
 }
 ?>
